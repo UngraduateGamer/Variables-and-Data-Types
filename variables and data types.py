732d6727cda611a80526366ff868cbb339cc295c
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 14:43:03 +0530

    Add files via upload

---
+++ variables and data types.py
@@ -0,0 +1,19 @@
+a=9
+b=15
+# concatenate the two integers
+print("The sum of a and b is ",a+b)
+
+a1='Rahul'
+a2='Kumar'
+# concatenate the two Strings
+print(a1+a2)
+
+# type function used for which type of data will be store
+print(type(a))
+
+# data types
+a=1        #integers
+b=1.2      #Float
+c="rahul"  #string
+d=True     #Booleans
+e=None     #None
